acc9a40c8dae59917279a5235da7a8b357513ca8
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-01 22:14:01 -0500

    Fixed a compilation issue in user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -25,5 +25,17 @@ const UserSchema = new mongoose.Schema({
      this.salt = this.makeSalt();
      this.hashed_password = this.encryptPassword(password)
  }).get(() => this._password);
- 
+
+ UserSchema.methods = {
+     authenticate: (plainText) => this.encryptPassword(plainText) === this.hashed_password,
+     encryptPassword: (password) => {
+         if (!password) return '';
+         try {
+             return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+         } catch(err) {
+             return '';
+         }
+     },
+     makeSalt: () => Math.round((new Date().valueOf() * Math.random())) + ''
+ }
 export default mongoose.model('User', UserSchema);
