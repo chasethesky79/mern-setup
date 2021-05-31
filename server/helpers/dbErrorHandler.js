70d465c6398546626865cac4c897276d9af9c86e
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-30 20:04:28 -0500

    Built the db error handler function

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -13,3 +13,17 @@ const getErrorMessage = (err) => {
         message = Object.values(err.errors).find(value => value?.message)?.message
     }
 }
+
+const getUniqueErrorMessage = (err) => {
+    let output;
+    const { message } = err;
+    try {
+        const fieldName = message?.substring(message?.lastIndexOf('.$') + 2, message?.lastIndexOf('_1'));
+        output = `${fieldName?.charAt(0).toUpperCase() + fieldName?.slice(1)} already exists`;
+    } catch(ex) {
+        output = 'Unique field already exists';
+    }
+    return output;
+}
+
+export default getErrorMessage;
