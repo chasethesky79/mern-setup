0275beaa008664270bd398f59b08bedf27cb73e1
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-31 10:56:04 -0500

    Implemented the user controller APIs for CRUD operations

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model';
 import getErrorMessage from '../helpers/dbErrorHandler';
+import merge from 'lodash.merge';
 
 const create = async (req, res) => {
     const user = new User(req.body);
@@ -9,10 +10,65 @@ const create = async (req, res) => {
             message: 'Successfully signed up !!'
         })
     } catch (err) {
-        return res.status(400).json({
-            error: getErrorMessage(err)
-        })
+        return handleError(res, err);
     }
 }
 
-export default { create }
+const list = async (req, res) => {
+    try {
+        const users = await User.find().select('name email updated created');
+        res.json(users);
+    } catch(err) {
+        return handleError(res, err);
+    }
+}
+
+const userByID = async (req, res, next, id) => {
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return handleError(res, `Could not retrieve user`);
+        }
+        req.profile = user
+        next()
+    } catch(err) {
+        return handleError(res, err);
+    }
+}
+
+const read = (req, res) => {
+    req.profile = merge(req.profile, { hashed_password: undefined, salt: undefined });
+    return res.json(req.profile);
+}
+
+const update = async (req, res) => {
+    try {
+        let user = req.profile;
+        user = merge(user, req.body, { updated: Date.now() });
+        await user.save();
+        user = merge(user, { hashed_password: undefined, salt: undefined });
+        return res.status(200).json(user);
+    } catch (err) {
+        return handleError(res, err);
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const user = req.profile;
+        const deletedUser = merge((await user.remove()), { hashed_password: undefined, salt: undefined });
+        return res.status(200).json(deletedUser);
+    } catch (err) {
+        return handleError(res, err);
+    }
+}
+
+function handleError(res, err) {
+    return res.status(400).json({
+        error: getErrorMessage(err)
+    });
+}
+
+export default { create, list, userByID, remove, read, update }
+
+
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,14 @@ import userCtrl from '../controllers/user.controller';
 const router = express.Router()
 
 router.route('/api/users')
-.post(userCtrl.create)
+    .post(userCtrl.create)
+    .get(userCtrl.list)
+
+router.route('/api/users/:userId')
+    .get(userCtrl.read)
+    .put(userCtrl.update)
+    .delete(userCtrl.remove)
+
+router.param('userId', userCtrl.userByID)
 
 export default router;
