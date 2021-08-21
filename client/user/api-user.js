ac6c95080cf77b8c07b467f922fa26694605dae5
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-08-21 14:33:57 -0500

    Implemented client side methods to read and update users

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -13,3 +13,34 @@ const create = async (user) => {
         console.log(err)
     }
 }
+
+const list = async (signal) => {
+    try {
+        const response = await fetch('/api/users', {
+            method: 'GET',
+            signal
+        })
+        return await response?.json();
+    }
+    catch(err) {
+       console.log(err);
+    }
+}
+
+const read = async (params, credentials, signal) => {
+    const { userId } = params;
+    try {
+        const response = await fetch(`/api/users/${userId}`, {
+            method: 'GET',
+            signal,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${credentials.t}`
+            }
+        });
+        return await response?.json();
+    } catch(err) {
+        console.log(err)
+    }
+}
